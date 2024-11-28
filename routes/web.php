8c12ba19f104d3b212d2b2ced4f4c1b874eba7ac
OmarAbed16 <OmarFathiAbed@gmail.com>
2024-11-28 11:14:38 +0300

    start cleaning code

--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,14 @@ Route::middleware('auth')->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('sign-out', [SessionsController::class, 'destroy'])->name('logout');
 
-    // Profile Routes
-    //Route::get('profile', [ProfileController::class, 'create'])->name('profile');
-    
+
     // Static Pages
-    Route::view('billing', 'pages.billing')->name('billing');
-    Route::view('rtl', 'pages.rtl')->name('rtl');
-    Route::view('virtual-reality', 'pages.virtual-reality')->name('virtual-reality');
-    Route::view('notifications', 'pages.notifications')->name('notifications');
-    Route::view('static-sign-in', 'pages.static-sign-in')->name('static-sign-in');
-    Route::view('static-sign-up', 'pages.static-sign-up')->name('static-sign-up');
+    // Route::view('billing', 'pages.billing')->name('billing');
+    // Route::view('rtl', 'pages.rtl')->name('rtl');
+    // Route::view('virtual-reality', 'pages.virtual-reality')->name('virtual-reality');
+    // Route::view('notifications', 'pages.notifications')->name('notifications');
+    // Route::view('static-sign-in', 'pages.static-sign-in')->name('static-sign-in');
+    // Route::view('static-sign-up', 'pages.static-sign-up')->name('static-sign-up');
 
 
     //MyProfile Pages
